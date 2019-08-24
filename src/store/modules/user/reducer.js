a31f594d94a66000ada164912cc13bd87f024ae5
rodrigodasilva <ribeiro.rodrigodasilva>
2019-08-24 19:26:06 -0300

    feat: logout da aplicacao

--- src/store/modules/user/reducer.js
+++ src/store/modules/user/reducer.js
@@ -16,6 +16,11 @@ export default function user(state = INITIAL_STATE, action) {
         draft.profile = action.payload.profile;
         break;
       }
+
+      case '@auth/SIGN_OUT': {
+        draft.profile = null;
+        break;
+      }
       default:
     }
   });
